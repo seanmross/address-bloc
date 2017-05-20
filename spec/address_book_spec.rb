97d092beab6e1cbab70745e71269f1787a844225
Sean Ross <seanmross9@gmail.com>
2017-05-19 23:37:42 -0400

    Assignment-6-csv: complete

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -49,41 +49,60 @@ RSpec.describe AddressBook do
        expect(book_size).to eq 5
      end
 
-   it "imports the 1st entry" do
+     it "imports the 1st entry" do
        book.import_from_csv("entries.csv")
        entry_one = book.entries[0]
-
-       expect(entry_one.name).to eq "Bill"
-       expect(entry_one.phone_number).to eq "555-555-4854"
-       expect(entry_one.email).to eq "bill@blocmail.com"
+       check_entry(entry_one, "Bill", "555-555-4854", "bill@blocmail.com")
      end
+
      it "imports the 2nd entry" do
-book.import_from_csv("entries.csv")
-# Check the second entry
-entry_two = book.entries[1]
+       book.import_from_csv("entries.csv")
+       entry_two = book.entries[1]
+       check_entry(entry_two, "Bob", "555-555-5415", "bob@blocmail.com")
+     end
 
-end
+     it "imports the 3rd entry" do
+       book.import_from_csv("entries.csv")
+       entry_three = book.entries[2]
+       check_entry(entry_three, "Joe", "555-555-3660", "joe@blocmail.com")
+     end
 
-it "imports the 3rd entry" do
-book.import_from_csv("entries.csv")
-# Check the third entry
-entry_three = book.entries[2]
+     it "imports the 4th entry" do
+       book.import_from_csv("entries.csv")
+       entry_four = book.entries[3]
+       check_entry(entry_four, "Sally", "555-555-4646", "sally@blocmail.com")
+     end
 
-end
+     it "imports the 5th entry" do
+       book.import_from_csv("entries.csv")
+       entry_five = book.entries[4]
+       check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
+     end
 
-it "imports the 4th entry" do
-book.import_from_csv("entries.csv")
-# Check the fourth entry
-entry_four = book.entries[3]
+     it "imports the correct number of entries" do
+       book.import_from_csv("entries_2.csv")
+       book_size = book.entries.size
 
-end
+       expect(book_size).to eq 3
+     end
 
-it "imports the 5th entry" do
-book.import_from_csv("entries.csv")
-# Check the fifth entry
-entry_five = book.entries[4]
+     it "imports the 1st entry" do
+       book.import_from_csv("entries_2.csv")
+       entry_one = book.entries[0]
+       check_entry(entry_one, "Anne", "444-555-3660", "anne@gmail.com")
+     end
 
-end
-end
+     it "imports the 2nd entry" do
+       book.import_from_csv("entries_2.csv")
+       entry_two = book.entries[1]
+       check_entry(entry_two, "Kylie", "333-555-5415", "kylie@gmail.com")
+     end
+
+     it "imports the 3rd entry" do
+       book.import_from_csv("entries_2.csv")
+       entry_three = book.entries[2]
+       check_entry(entry_three, "Sean", "222-555-4854", "sean@gmail.com")
+     end
+   end
 
 end
